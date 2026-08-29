63fdd8478c8b6a4fe1c435b6b1caaf72c996ed41
SaiDivakar-Navara <22p31a0524@acet.ac.in>
2026-08-29 16:24:59 +0530

    Updated

--- Project files/Backend/db/connect.js
+++ Project files/Backend/db/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const db= 'Your MongoDB Connection String Here'
+const db= 'mongodb+srv://nani:GxqCT1VPqTzjU1ub@shopsmart.nlqzspn.mongodb.net/?appName=ShopSmart'
 // Connect to MongoDB using the connection string
 
 mongoose.connect(db,{
